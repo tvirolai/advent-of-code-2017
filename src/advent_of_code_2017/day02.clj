f8d1c5309bba194ec01b5938ed9ecbc7cc34472e
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-12-02 22:10:53 +0200

    Day 2

---
+++ src/advent_of_code_2017/day02.clj
@@ -0,0 +1,29 @@
+(ns advent-of-code-2017.day02
+  (:require [clojure.string :as s]))
+
+
+(def input
+  (slurp "./data/day02.txt"))
+
+(defn parse-from-string [string]
+  (->> (s/split input #"\n")
+       (map #(s/split % #"\t"))
+       (map (fn [line] (reverse (sort (map #(Integer. %) line)))))))
+
+(defn part-1 [input]
+  (->> (parse-from-string input)
+       (map #(- (first %) (last %)))
+       (reduce +)))
+
+(defn solve-line [line]
+  (let [res (for [x (rest line)
+                  :when (zero? (rem (first line) x))]
+              (/ (first line) x))]
+    (if (empty? res)
+      (solve-line (rest line))
+      (first res))))
+
+(defn part-2 [input]
+  (->> (parse-from-string input)
+       (map solve-line)
+       (reduce +)))
--- test/advent_of_code_2017/core_test.clj
+++ test/advent_of_code_2017/core_test.clj
@@ -1,7 +1,8 @@
 (ns advent-of-code-2017.core-test
   (:require [clojure.test :refer :all]
             [advent-of-code-2017.core :refer :all]
-            [advent-of-code-2017.day01 :as day01]))
+            [advent-of-code-2017.day01 :as day01]
+            [advent-of-code-2017.day02 :as day02]))
 
 (deftest examples
   (testing "Day 1, part 1"
@@ -15,3 +16,7 @@
     (is (= (day01/part-2 "123425") 4))
     (is (= (day01/part-2 "123123") 12))
     (is (= (day01/part-2 "12131415") 4))))
+
+(deftest day2
+  (testing "Day 2, part 1"
+    (is (= (day02/part-1 "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8") 18))))
