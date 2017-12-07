6ab4624c62adfc6a6b710b87e3b0f42ac8936bf1
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-12-07 22:58:05 +0200

    Add test coverage

--- test/advent_of_code_2017/day05_test.clj
+++ test/advent_of_code_2017/day05_test.clj
@@ -10,6 +10,9 @@
     (vector? (d5/load-input)))
   (testing "Day 5, part 1 with the example input"
     (is (= 5 (d5/solve example-input 1))))
+  (testing "Day 5, part 1 with weird values"
+    (is (= 1 (d5/solve [4] 1)))
+    (is (= 0 (d5/solve [] 1))))
   (testing "Day 5, part 1 with the puzzle input"
     (is (= 376976 (d5/solve (d5/load-input) 1))))
   (testing "Day 5, part 2 with the example input"
