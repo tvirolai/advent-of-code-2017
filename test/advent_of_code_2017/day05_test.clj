8b10b316a17f243cfe68bfea2d64439523ae25c2
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-12-07 22:52:48 +0200

    Add test coverage

--- test/advent_of_code_2017/day05_test.clj
+++ test/advent_of_code_2017/day05_test.clj
@@ -6,7 +6,11 @@
   [0 3 0 1 -3])
 
 (deftest day05
+  (testing "Helper functions"
+    (vector? (d5/load-input)))
   (testing "Day 5, part 1 with the example input"
     (is (= 5 (d5/solve example-input 1))))
+  (testing "Day 5, part 1 with the puzzle input"
+    (is (= 376976 (d5/solve (d5/load-input) 1))))
   (testing "Day 5, part 2 with the example input"
     (is (= 10 (d5/solve example-input 2)))))
